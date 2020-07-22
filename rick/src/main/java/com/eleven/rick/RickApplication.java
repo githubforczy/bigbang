2938515f25595ad9c4d62d394a07f3b76c1c36f5
czy <516790759@qq.com>
2020-07-22 16:18:25 +0800

    #新建项目

---
+++ rick/src/main/java/com/eleven/rick/RickApplication.java
@@ -0,0 +1,13 @@
+package com.eleven.rick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickApplication.class, args);
+    }
+
+}
